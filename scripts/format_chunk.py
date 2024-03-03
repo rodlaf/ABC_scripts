4067bdfffae7bddccce4fdc1de727e418e3087a9
Rodney Lafuente <rodneyslafuente@gmail.com>
2024-03-03 19:19:05 +0000

    add example yaml processing

--- scripts/format_chunk.py
+++ scripts/format_chunk.py
@@ -5,6 +5,7 @@ from tqdm import tqdm
 from multiprocessing import Pool
 import time
 from functools import partial
+import yaml
 
 TOPLEVEL_DIR = "/root/ABC_scripts"
 BASH_SCRIPTS_DIR = f"{TOPLEVEL_DIR}/scripts"
@@ -12,27 +13,37 @@ LOG_DIR = f"{TOPLEVEL_DIR}/logs"
 DATASET_DIR = f"{TOPLEVEL_DIR}/dataset"
 
 
-def write_line_36(filename: str, meta_dir_path: str) -> None:
-    with open(os.path.join(meta_dir_path, filename), "r") as f:
-        content = f.readlines()
-        with open(os.path.join(os.getcwd(), "output.txt"), "a") as f2:
-            f2.write(content[36])
+def process_yaml_file(filename: str, meta_dir_path: str, output_file_path: str) -> None:
+    with open(os.path.join(meta_dir_path, filename), "r") as yaml_file:
+        meta_json: dict = yaml.safe_load(yaml_file)
+
+        with open(os.path.join(os.getcwd(), output_file_path), "a") as output_file:
+            output_file.write(str(meta_json))
 
 
 def main(args: argparse.Namespace) -> None:
     meta_dir_path = os.path.join(DATASET_DIR, f"meta_extracted_{str(args.chunk_num)}")
+    output_file_path = "output.txt"
+
+    if os.path.exists(output_file_path):
+        os.remove(output_file_path)
+    open(output_file_path, "w")
 
     start = time.time()
 
     if args.parallel == 1:
         print("Using parallelization.")
 
-        unary = partial(write_line_36, meta_dir_path=meta_dir_path)
+        unary = partial(
+            process_yaml_file,
+            meta_dir_path=meta_dir_path,
+            output_file_path=output_file_path,
+        )
         filename_list = os.listdir(meta_dir_path)
 
         with Pool() as pool:
             for _ in tqdm(
-                pool.imap_unordered(unary, filename_list, chunksize=512),
+                pool.imap_unordered(unary, filename_list, chunksize=1),
                 total=len(filename_list),
             ):
                 pass
@@ -41,7 +52,7 @@ def main(args: argparse.Namespace) -> None:
         print("Not using parallelization.")
 
         for filename in tqdm(os.listdir(meta_dir_path)):
-            write_line_36(filename, meta_dir_path)
+            process_yaml_file(filename, meta_dir_path, output_file_path)
 
     end = time.time()
     print("Elapsed time: ", end - start)
