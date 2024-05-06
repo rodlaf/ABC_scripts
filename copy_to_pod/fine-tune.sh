35a64efd6674a0fa47023e5a49a14898821fdaec
Rodney Lafuente Mercado <rodneyslafuente@gmail.com>
2024-05-06 06:41:07 +0000

    found config that works

--- copy_to_pod/fine-tune.sh
+++ copy_to_pod/fine-tune.sh
@@ -4,4 +4,7 @@ tar -xvf /workspace/copy_to_pod/small_data.jsonl.tar.gz -C /workspace/copy_to_po
 
 /workspace/copy_to_pod
 cd /workspace/axolotl
-accelerate launch -m axolotl.cli.train /workspace/copy_to_pod/config.yml
+
+# PYTORCH_CUDA_ALLOC_CONF=garbage_collection_threshold:0.6,max_split_size_mb:128 
+# https://gist.github.com/ashmalvayani/b4dee2084ffac9dbddc5dd32353448d3
+accelerate launch -m axolotl.cli.train /home/rslm/ABC_scripts/copy_to_pod/config.yml &> ../ft.log
